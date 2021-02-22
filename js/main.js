446d92509f02a678c27e683207a6d1233997e96b
katyoverstreet <77992431+katyoverstreet@users.noreply.github.com>
2021-02-22 06:29:13 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,88 +1,31 @@
-<head> 
-    <title> Digital Clock </title>
-    <link rel="stylesheet" href="css/style.css">
-    
-</head>
 
 
-<body onload="setInterval(displayTime(), 1000)">
-    <div id="digitalClockTime" class="clock"> </div>
-    <script src="js/main.js"></script> 
-</body>
+function displayTime() {
+    let date = new Date()
+    let hour = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let amORpm = "AM";
 
-
-/*
-const clock = document.getElementById('clock'); //creating clock var and setting its value
-
-const setTwelve = document.querySelector('.btn');
-
-const setTwentyFour = document.querySelector('.btn');
-
-setTwelve.addEventListener('click', makeNormal);
-
-setTwentyFour.addEventListener('click', makeMilitary);
-
-let militaryTime = false; //setting value for milt time
-
- function makeMilitary() {
-    if (militaryTime === false) {
-        militaryTime = true;
+    if (hour < 12) {
+        amORpm = "AM";
     }
+    if (hour > 12) {
+        hour -= 12;
+        amORpm = "PM";
+    }
+	//simpler than if statements  asically if less than ten, add 0 to beginning, else just put value
+    hour = hour < 10 ? "0" + hour : hour; 
+    mins = mins < 10 ? "0" + mins : mins;
+    secs = secs < 10 ? "0" + secs : secs;
+
+
+    let currentTime = hour + ":" + mins + ":" + secs + amORpm
+
+
+    document.getElementById("digitalClockTime")
+        .innerHTML = currentTime
 }
-//if its pushed make it true
-function makeNormal() {
-    if (militaryTime === true) {
-        militaryTime = false;
-    }
-
-}
-setInterval(displayClock, 1000);
-//if normal button is pushed make it false
-function displayClock() {
-
-    let currentTime = new Date();
-    let tfHours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
-    let amOrPm = '';
-    
-    if (tfHours < 12) {
-        amOrPm = 'AM';
-    }
-    else{
-        amOrPm = 'PM';
-    }
-    
-    
-    if (tfHours > 12 & militaryTime === false) {
-        tfHours = (24 - tfHours) - 12;
-    }
-
-    hourstw = Math.abs(tfHours);
-    
-    //just adding 0s
-    if (hourstw < 10) {
-        hourstw = '0' + hourstw; //this makes it so when it is, for example 9:09:09, it puts the 0 before each
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    if (seconds < 10) {
-        seconds = '0' + seconds;
-    }
-
-	//this whole section is just adding 0s
-  
-    let timeNow = hourstw + ':' + minutes + ':' + seconds + amOrPm;
-    
-    clock.innerHTML = timeNow;
-  //making the output show the work
-    
-}
-
-*/
-
- displayClock(); 
 
 
 
